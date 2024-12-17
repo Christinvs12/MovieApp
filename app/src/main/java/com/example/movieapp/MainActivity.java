1450bdeb4efa35ce1c61fc7911075e701d272102
Christinvs12 <christinvs2004@gmail.com>
2024-12-17 11:26:25 +0530

    project created

---
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.movieapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et1,et2;
+    Button b1;
+    String getuname,getpass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        et1=(EditText) findViewById(R.id.uname);
+        et2=(EditText) findViewById(R.id.pass);
+        b1=(Button) findViewById(R.id.log);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getuname=et1.getText().toString();
+                getpass=et2.getText().toString();
+            }
+        });
+
+        };
+    }
